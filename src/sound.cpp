3151a09b272f3d69758f942282baf53dee1e1258
Che-Huai Lin <lzh9102@gmail.com>
2016-01-11 22:05:45 +0800

    Prevent generating zombie mplayer processes

--- src/sound.cpp
+++ src/sound.cpp
@@ -11,21 +11,31 @@ namespace
 {
 	void mplayer(const char *filename)
 	{
-		int pid = fork();
-		if (pid == 0) {
-			int devnull = open("/dev/null", O_WRONLY);
+		int tmp_pid = fork();
+		// Use double forking to reparent the inner child process to init.
+		// This frees us from having to block to wait() for the child process.
+		if (tmp_pid == 0) {
+			int pid = fork();
+			if (pid == 0) {
+				int devnull = open("/dev/null", O_WRONLY);
 
-			// redirect stdout and stderr to /dev/null
-			close(1);         // close stdout
-			close(2);         // close stderr
-			dup2(devnull, 1); // devnull -> stdout
-			dup2(devnull, 2); // devnull -> stderr
-			close(devnull);   // close devnull
+				// redirect stdout and stderr to /dev/null
+				close(1);         // close stdout
+				close(2);         // close stderr
+				dup2(devnull, 1); // devnull -> stdout
+				dup2(devnull, 2); // devnull -> stderr
+				close(devnull);   // close devnull
 
-			execlp("mplayer", "mplayer", filename, NULL);
-			cerr << "error: failed to execute mplayer" << endl;
-			exit(1);
+				execlp("mplayer", "mplayer", filename, NULL);
+				cerr << "error: failed to execute mplayer" << endl;
+				exit(1);
+			}
+
+			// Exit the outer child process so that the parent of the inner child
+			// process will be set to init.
+			exit(0);
 		}
+		waitpid(tmp_pid, NULL, 0);
 	}
 }
 
