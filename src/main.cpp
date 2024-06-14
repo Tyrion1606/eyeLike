686e5dfc6b6dadd3be17bf5eaf12cb69f6aea6ac
Davi Monteiro Alves <94870055+Tyrion1606@users.noreply.github.com>
2024-06-14 09:39:02 -0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-#include <opencv2/objdetect/objdetect.hpp>
+#include "opencv2/opencv.hpp"
+#include "opencv2/objdetect.hpp"
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
@@ -28,7 +29,7 @@ void detectAndDisplay( cv::Mat frame );
 
 /** Global variables */
 //-- Note, either copy these two files from opencv/data/haarscascades to your current folder, or change these locations
-cv::String face_cascade_name = "../res/haarcascade_frontalface_alt.xml";
+cv::String face_cascade_name = "../../../res/haarcascade_frontalface_alt.xml";
 cv::CascadeClassifier face_cascade;
 std::string main_window_name = "Capture - Face detection";
 std::string face_window_name = "Capture - Face";
